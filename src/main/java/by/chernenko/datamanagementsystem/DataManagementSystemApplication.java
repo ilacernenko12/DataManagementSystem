e19ec71ae8853494a7d4a8b3faf6a4bad4c559ab
Ilya Chernenko <ilacernenko12@gmail.com>
2024-12-04 18:11:22 +0300

    Initial commit

---
+++ src/main/java/by/chernenko/datamanagementsystem/DataManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package by.chernenko.datamanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/chernenko/datamanagementsystem/DataManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package by.chernenko.datamanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
